04b060c5b5631d5c37b125447bc187c79f8c7ec0
GabrielBSL-UEA <gbsl.snf17@uea.edu.br>
2021-01-28 18:57:30 -0400

    Controle do jogador

---
+++ GlobalGameJam2021/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField] PlayerMovement player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetIsGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetIsGrounded(false);
+        }
+    }
+}
