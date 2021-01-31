f9b73165673bca8178c117943b90abcb6e8b8354
GabrielBSL-UEA <gbsl.snf17@uea.edu.br>
2021-01-31 09:46:41 -0400

    Novos sounds

---
+++ GlobalGameJam2021/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Serializable]
+    class LevelMusic
+    {
+        public string name;
+        public AudioClip music;
+    }
+
+    [SerializeField] LevelMusic[] musics;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        MusicManager[] objects = FindObjectsOfType<MusicManager>();
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if(objects[i].gameObject != gameObject)
+            {
+                Destroy(objects[i].gameObject);
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void changeMusic(string name)
+    {
+        for (int i = 0; i < musics.Length; i++)
+        {
+            if(musics[i].name == name)
+            {
+                audioSource.clip = musics[i].music;
+                break;
+            }
+        }
+    }
+
+    public void FadeOutMusic(float duration)
+    {
+        StartCoroutine(MusicFadeOut(0, duration));
+    }
+
+    public void FadeInMusic(float duration)
+    {
+        StartCoroutine(MusicFadeIn(1, duration));
+    }
+
+    IEnumerator MusicFadeIn(int goTo, float duration)
+    {
+        while (audioSource.volume < 1)
+        {
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, 1, Time.deltaTime / duration);
+            yield return null;
+        }
+
+        audioSource.Play();
+    }
+
+    IEnumerator MusicFadeOut(int goTo, float duration)
+    {
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, 0, Time.deltaTime / duration);
+            yield return null;
+        }
+    }
+}
