17d6815eb8d11f7a5f263273faf625ce2af88190
Suziane Cundiff <suzianecundiff@gmail.com>
2021-01-31 23:14:12 -0400

    Menu principal adicionado

---
+++ GlobalGameJam2021/Assets/Scripts/ReturnButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ReturnButton : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject button;
+    EventSystem eventSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // mainMenu = GameObject.Find("MainMenu");
+        // Debug.Log(mainMenu);
+        // button = GameObject.Find("Button_" + gameObject.name);
+        eventSys = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+            eventSys.SetSelectedGameObject(button);
+        }
+    }
+}
